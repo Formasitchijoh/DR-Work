617345292b64ee60f306221c0a04136fe56ed35b
Formasitchijoh <formasitf@gmail.com>
2023-07-20 18:40:30 +0100

    implemented seach/filter by freetext and by category

---
+++ src/Components/Category.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import entries from './Data/data'
+const Category = () => {
+  return (
+    <div className='absolute w-1/2 h-1/2'>
+
+    </div>
+  )
+}
+
+export default Category
